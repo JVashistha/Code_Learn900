d35d48f7cc22e6022184d051d478ccd451511b39
JVashistha <91934832+JVashistha@users.noreply.github.com>
2023-07-17 09:14:02 +0000

    comparisons

---
+++ .devcontainer/Basics_01/04_comparisons.js
@@ -0,0 +1,22 @@
+console.log(2>=1);
+console.log(2>1);
+console.log(2==1);
+console.log(2!=1);
+
+// avoid  these types of comparisons as conversion starts to play here 
+// you never know what is inside this comparison
+// you need to check it by console log to cnfim it and that is time consuming 
+
+// comparison with null
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// comparion with undefined
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
