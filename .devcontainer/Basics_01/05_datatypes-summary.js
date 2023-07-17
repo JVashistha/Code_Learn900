14d5b13ed1676dcf9154fc3f81c2a2241881be74
JVashistha <91934832+JVashistha@users.noreply.github.com>
2023-07-17 09:14:37 +0000

    datatypes

---
+++ .devcontainer/Basics_01/05_datatypes-summary.js
@@ -0,0 +1,27 @@
+// Primitive datatype
+
+// 7 types:
+String, Number, Null, Undefined, symbol, Boolean, BigInt
+
+// Reference type or Non-Primitive
+
+// Array, Object, Functions
+
+const outsidetemp = null;
+
+// use of symbol
+
+const id = symbol('123');
+const anotherId = symbol('123');
+console.log(id === anotherId);
+
+// Bigint
+
+bigNumber = 12345678956246839754789654231n
+
+const heros = ["Shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "jitender",
+    age:22,
+}
